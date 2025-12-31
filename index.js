e6c25d9521d413beb0190661277e37e9c7c59025
unknown <refaatnashabeh@gmail.com>
2025-12-31 22:19:41 +0200

    db connection

--- index.js
+++ index.js
@@ -35,13 +35,12 @@ const verifyToken = (req, res, next) => {
 
 // Database connection
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bookstore",
-  port: 3307
-}
-)
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306),
+});
 
 // Books Route 
 app.get("/books", (req, res) => {
